f5a8fe6c8ee138b971d17d3a646bba1d48b870b1
Shruti Bendale <shrutibendale@gmail.com>
2023-02-11 22:30:51 -0800

    Update contact links in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import './footer.css'
-import { FaInstagram, FaFacebookF , FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaMediumM , FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <section id="footer">
-        <div className="containier footer footer_container">
-            <a href="https://shrutiandskye.com/#" className='footer_logo'>Shruti</a> 
+        <div className="footer footer_container">
+            <a href="#" className='footer_logo'>Shruti</a> 
             <ul className='permalinks'>
-                    <li><a href = 'https://shrutiandskye.com/#'>Home</a></li>
-                    <li><a href = 'https://shrutiandskye.com/#projects'>Projects</a></li>
-                    <li><a href = 'https://shrutiandskye.com/#contact'>Contact Me</a></li>
+                    <li><a href = '#'>Home</a></li>
+                    <li><a href = '#projects'>Projects</a></li>
+                    <li><a href = 'mailto:shrutibendale@gmail.com'>Contact Me</a></li>
             </ul>
 
             <div className="footer_socials">
-                    <a href="https://github.com/ShrutiBendale"><FaGithub/></a>
-                    <a href="https://www.linkedin.com/in/shrutibendale"><FaLinkedin/></a>
-                    <a href="https://www.instagram.com/shrutibendale/"><FaInstagram/></a>
-                    <a href="https://www.facebook.com/shruti.bendale.3/"><FaFacebookF/></a>
-                    
+                    <a href="https://github.com/ShrutiBendale" target="_blank"><FaGithub/></a>
+                    <a href="https://www.linkedin.com/in/shrutibendale" target="_blank"><FaLinkedin/></a>
+                    <a href="https://medium.com/@shrutibendale" target="_blank"><FaMediumM/></a>
             </div>
         </div>
 
